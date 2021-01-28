d7e7bfd66eedaa9fcb6491e3f40dbc382427d94e
ShalikoHayrapetyan <73520604+ShalikoHayrapetyan@users.noreply.github.com>
2021-01-28 23:49:13 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,24 @@ const input=document.getElementById("input")
 const add=document.getElementById("add")
 const list=document.getElementById("list")
 const del=document.getElementsByClassName("del")
-const btnRemove=document.createElement("button")
-btnRemove.className=("del")
-btnRemove.textContent="\u00D7"
+
 for (i = 0; i < del.length; i++) {
     del[i].onclick = deleteMe
 }
 
 add.addEventListener("click",()=>{
+const btnRemove=document.createElement("button")
+btnRemove.className=("del")
+btnRemove.textContent="\u00D7"
+btnRemove.onclick = deleteMe
     let li=document.createElement("li")
     if(input.value.length<=2){
         alert ("Please write longer text")
     }else if(input.value.length>40){
         alert ("Your text is very long")
     }else{
-        li.textContent=input.value 
-        let myBtn=btnRemove.cloneNode(true)
-        myBtn.onclick = deleteMe
-        li.append(myBtn)
+        li.textContent=input.value
+        li.append(btnRemove)
         list.append(li)
         input.value=""
     }
